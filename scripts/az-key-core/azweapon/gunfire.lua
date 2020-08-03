4cf44e38da280b45dbe1ae2d98789d163ab90829
Azulaloi Champagne <azulaloichampagne@gmail.com>
2020-08-03 18:44:35 -0400

    GunFire refactored behavior

--- scripts/az-key-core/azweapon/gunfire.lua
+++ scripts/az-key-core/azweapon/gunfire.lua
@@ -1,7 +1,9 @@
 require "/scripts/util.lua"
 require "/scripts/interp.lua"
 
--- Base gun fire ability
+-- AzCore GunFire (key)
+-- I don't even wanna hear it about the mixed tabs. Blame notepad++. 
+
 GunFire = WeaponAbility:new()
 
 function GunFire:init()
@@ -12,12 +14,74 @@ function GunFire:init()
   self.weapon.onLeaveAbility = function()
     self.weapon:setStance(self.stances.idle)
   end
-  
+ 
   -- class loader test
   --if gunLoad ~= nil then
   --  sb.logInfo(tostring(gunLoad.testval))
   --end
+  
+  self:initParameters()
 end
+
+function GunFire:initParameters()
+  -- abilities automatically add all configured parameters to self table
+  -- so we must set defaults for manually
+  
+  -- the pattern: '((param ~= nil) and param) or default' sets param to default if nil. 
+  -- nil evaluates to false as boolean so extra verbage is necessary when setting boolean with pseudo-ternary
+  self.triggerMode = self.triggerMode or "auto"
+  self.actionMode = self.actionMode or "single"
+  self.burstTerm = ((self.burstTerm ~= nil) and self.burstTerm) or true  
+  
+  self.chargeMode = ((self.chargeMode ~= nil) and self.chargeMode) or false 
+  self.chargeStore = self.chargeStore or 0
+  self.chargeDecay = self.chargeDecay or 1
+  self.chargeMax = self.chargeMax or 2
+  
+  self.chargeTimer = self.chargeMax
+  
+  -- internal values
+  self.fireHeld = false
+  self.firedFor = false
+  self.charging = false
+  self.charged = false
+end
+
+----
+-- TEMP DOCS (easier to reference here while working)
+-- Config Parameters
+
+-- self.triggerMode = "semi", "auto"
+--   "semi" : trigger must be pressed for each actuation
+--   "auto" : will continuously actuate while trigger is held
+
+-- self.actionMode = "single", "burst"
+--   "single" : each actuation will fire once
+--   "burst"  : each actuation will fire for burstCount
+
+-- self.burstTerm = boolean
+--   true  : burst can be terminated by releasing trigger
+--   false : burst will continue regardless of trigger state
+
+-- self.chargeMode = boolean
+--   toggles charge functionality
+
+-- self.chargeStore = boolean
+--   when charge state ends, 
+--     0 : charge will reset
+--     1 : charge will remain, but reset when action ends
+--     2 : charge will remain even when action ends
+
+-- self.chargeDecay = float
+--   if chargeStore, charge will decay by (this * dt) while not charging
+-----
+
+--todo:
+-- restore charge stance behavior
+-- charge FX
+-- charge burst functionality
+-- burst should be able to optionally play a single effect on actuation instead of multiple shot effects
+-- charge/non-charge hybrid behavior (like the vanilla dragonfire pistol)
 
 function GunFire:update(dt, fireMode, shiftHeld)
   WeaponAbility.update(self, dt, fireMode, shiftHeld)
@@ -27,29 +91,96 @@ function GunFire:update(dt, fireMode, shiftHeld)
   if animator.animationState("firing") ~= "fire" then
     animator.setLightActive("muzzleFlash", false)
   end
-
-  if self.fireMode == (self.activatingFireMode or self.abilitySlot)
+  
+  local flagFire = fireMode == (self.activatingFireMode or self.abilitySlot)
+  self.fireHeld = flagFire
+  
+  if flagFire
     and not self.weapon.currentAbility
     and self.cooldownTimer == 0
     and not status.resourceLocked("energy")
     and not world.lineTileCollision(mcontroller.position(), self:firePosition()) 
 	then
-
-    if self.fireType == "auto" and status.overConsumeResource("energy", self:energyPerShot()) then
-      self:setState(self.auto)
-    elseif self.fireType == "burst" then
-      self:setState(self.burst)
-    elseif self.fireType == "charge" then
-	  self:setState(self.charge)
+	
+	local flagSemi = (self.triggerMode == "semi" and not self.firedFor) or (self.triggerMode ~= "semi")
+	
+	if flagSemi and self.chargeMode then
+		if self.charged then
+			if self.actionMode == "single" and status.overConsumeResource("energy", self:energyPerShot()) then
+				self:setState(self.fire)
+			end
+			-- CHARGE BURST BEHAVIOR GOES HERE
+		else
+			self:setState(self.charge)
+		end
+	elseif flagSemi then
+		if self.actionMode == "single" and status.overConsumeResource("energy", self:energyPerShot()) then
+			self:setState(self.fire)
+		elseif self.actionMode == "burst" then
+			self:setState(self.burst)
+		end
 	end
   end
+  
+  if not self.fireHeld then
+	self.firedFor = false
+	
+	if self.charged then -- CHARGE ACTION COMPLETED
+		self.charged = false
+		if (self.chargeStore < 2) then
+			self.chargeTimer = self.chargeMax
+		end
+	end
+	
+	if self.charging then -- CHARGE ABORTED
+		self.charging = false 
+		if (self.chargeStore == 0) then
+			self.chargeTimer = self.chargeMax
+		end
+	end
+  end
+
+  -- decay charge timer
+  if (not (self.charging or self.charged)) and (self.chargeStore > 0) then
+	self.chargeTimer = math.min(self.chargeTimer + (dt * self.chargeDecay), self.chargeMax)
+  end
+  
+  self:drawDebug()
 end
 
-function GunFire:auto()
+function GunFire:drawDebug()
+	local pos = mcontroller.position()
+	
+	world.debugText("trigger:  " .. self.triggerMode, vec2.add(pos, {4, 2}), "green")
+	world.debugText("action:   " .. self.actionMode, vec2.add(pos, {4, 1.5}), "green")
+	world.debugText("cooldown: " .. round(self.cooldownTimer, 2), vec2.add(pos, {4, 1}), "green")
+	
+	local str0 = self.chargeMode and "true" or "false"
+	local str00 = self.chargeMode and "" or (", " .. tostring(self.chargeStore))
+	world.debugText("charge:  " .. str0 .. str00, vec2.add(pos, {8.5, 2}), "green")
+	
+	if self.chargeMode then
+		local str1 = self.charging and "true" or "false"
+		world.debugText("chrging: " .. str1, vec2.add(pos, {8.5, 1.5}), "green")
+	
+		world.debugText("chTimer: " .. round(self.chargeTimer, 2), vec2.add(pos, {8.5, 1}), "green")
+	
+		local str2 = self.charged and "true" or "false"
+		world.debugText("charged: " .. str2, vec2.add(pos, {8.5, 0.5}), "green")
+	end
+end
+
+----
+-- FIRE STATES
+----
+
+function GunFire:fire()
   self.weapon:setStance(self.stances.fire)
 
   self:fireProjectile()
   self:muzzleFlash()
+  
+  self.firedFor = true
 
   if self.stances.fire.duration then
     util.wait(self.stances.fire.duration)
@@ -62,8 +193,12 @@ end
 function GunFire:burst()
   self.weapon:setStance(self.stances.fire)
 
+  local termFlag = (self.burstTerm and self.fireHeld) or (not self.burstTerm)
   local shots = self.burstCount
-  while shots > 0 and status.overConsumeResource("energy", self:energyPerShot()) do
+  while shots > 0 and 
+		status.overConsumeResource("energy", self:energyPerShot()) and
+		termFlag do
+	
     self:fireProjectile()
     self:muzzleFlash()
     shots = shots - 1
@@ -78,40 +213,29 @@ function GunFire:burst()
 end
 
 function GunFire:charge()
-  self.weapon:setStance(self.stances.charge)
+  --self.weapon:setStance(self.stances.charge)
+  self.weapon:setStance(self.stances.fire)
   
-  local chargeTimer = self.stances.charge.duration
+  --local chargeTimer = self.stances.charge.duration
+  if self.chargeStore <= 1 then
+	self.chargeTimer = self.chargeMax
+  end
   
-  while chargeTimer > 0 and self.fireMode == (self.activatingFireMode or self.abilitySlot) do
-	chargeTimer = chargeTimer - self.dt
-	
-	world.debugText(sb.print("Charge: " .. chargeTimer), vec2.add(mcontroller.position(), {1, 2}), "green")
+  while self.chargeTimer > 0 and self.fireMode == (self.activatingFireMode or self.abilitySlot) do
+    self.charging = true
+	self.chargeTimer = self.chargeTimer - self.dt
 	
 	coroutine.yield()
   end
   
-  if chargeTimer <= 0 then
+  if self.chargeTimer <= 0 then
+	self.charging = false
+	self.charged = true
 	self:setState(self.fire)
   else 
+	self.charging = false
 	self:setState(self.cooldown)
   end
- 
-end
-
--- this is identical to auto()
--- but will be used by other fire modes in the future
-function GunFire:fire()
-  self.weapon:setStance(self.stances.fire)
-  
-  self:fireProjectile()
-  self:muzzleFlash()
-  
-  if self.stances.fire.duration then
-	util.wait(self.stances.fire.duration)
-  end
-  
-  self.cooldownTimer = self.fireTime
-  self:setState(self.cooldown)
 end
 
 function GunFire:cooldown()
@@ -130,6 +254,10 @@ function GunFire:cooldown()
     progress = math.min(1.0, progress + (self.dt / self.stances.cooldown.duration))
   end)
 end
+
+----
+-- OTHER
+----
 
 function GunFire:muzzleFlash()
   animator.setPartTag("muzzleFlash", "variant", math.random(1, 3))
@@ -204,4 +332,12 @@ function GunFire:uninit()
     --    world.sendEntityMessage(id, "return")
     --  end
     --end
+end
+
+----
+-- UTIL
+----
+
+function round(num, dec)
+	return string.format("%." .. (dec or 0) .. "f", num)
 end
